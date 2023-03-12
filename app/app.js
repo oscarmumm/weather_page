0a7001237c51a5263acfd436e9ab4c88d9b5395f
Oscar Mumm <oscarmumm@gmail.com>
2023-03-12 17:01:00 -0300

    testing structure

--- app/app.js
+++ app/app.js
@@ -1,4 +1,14 @@
 const apiKey = "1e0852abb42466a00a4d6d999c3e2b64";
+const d = document;
+const currentWeather = d.getElementById("current-weather");
+const cityName = d.getElementById("city-name");
+const weatherIcon = d.getElementById("weather-icon");
+const temperature = d.getElementById("temperature");
+const feelsLike = d.getElementById("feels-like");
+const condition = d.getElementById("condition");
+const humidity = d.getElementById("humidity");
+const pressure = d.getElementById("pressure");
+
 
 // Obteniendo la ubicación del usuario
 const options = {
@@ -13,11 +23,15 @@ function success(pos) {
   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=${apiKey}&units=metric&lang=es`)
   .then((response) => response.json())
   .then((data) => console.log(data));
-}
+};
 
 function error(err) {
   console.warn(`ERROR(${err.code}): ${err.message}`);
-}
+};
 
 navigator.geolocation.getCurrentPosition(success, error, options);
 
+const showData = (data) => {
+  cityName.innerText = data.name;
+  weatherIcon = 0;
+}
