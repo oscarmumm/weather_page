d8185d76bcccba83f23b6d6765d2e9f12eb605ef
Oscar Mumm <oscarmumm@gmail.com>
2023-03-15 15:45:49 -0300

    funcionalidad inicial y diseño

--- app/app.js
+++ app/app.js
@@ -8,6 +8,8 @@ const feelsLike = d.getElementById("feels-like");
 const condition = d.getElementById("condition");
 const humidity = d.getElementById("humidity");
 const pressure = d.getElementById("pressure");
+const visibility = d.getElementById("visibility");
+const wind = d.getElementById("wind");
 
 
 // Obteniendo la ubicación del usuario
@@ -22,7 +24,7 @@ function success(pos) {
   console.log("location ready");
   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=${apiKey}&units=metric&lang=es`)
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => showData(data));
 };
 
 function error(err) {
@@ -33,5 +35,12 @@ navigator.geolocation.getCurrentPosition(success, error, options);
 
 const showData = (data) => {
   cityName.innerText = data.name;
-  weatherIcon = 0;
-}
+  weatherIcon.src = `./icons/${data.weather[0].icon}.svg`;
+  temperature.innerText = `${Math.round(data.main.temp)}°C`;
+  feelsLike.innerText = `ST: ${Math.round(data.main.feels_like)}°C`;
+  condition.src = data.weather[0].description;
+  humidity.innerText = `${data.main.humidity} %`;
+  pressure.innerText = `${data.main.pressure} hpa`;
+  visibility.innerText = `${(data.visibility)/1000} km`;
+  wind.innerText = data.wind.speed;
+};
